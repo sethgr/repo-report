a9e06027962d7560a6dc4f98f2b95696a4224bac
Joe Martella <martellaj@live.com>
2015-10-12 10:23:25 -0700

    App complete.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Repo Report
+
+**Repo Report** is an Office Add-in built for Outlook that scans your emails for GitHub repository links and displays information about the ones it finds.
+
+## How to run
+
+1. Clone this repository.
+2. 
+
+RegEx activation - https://msdn.microsoft.com/en-us/library/office/fp142135.aspx
--- appread/home/home.controller.js
+++ appread/home/home.controller.js
@@ -8,55 +8,23 @@
    * Controller constructor
    */
   function homeController(dataService){
-    var vm = this;  // jshint ignore:line
+    var vm = this;
 
-    vm.repos = [
-      {
-        'title': 'repo-stats',
-        'description': 'A sample Office Add-in (built with Angular) that scans emails for links to GitHub repositories and then fetches information about them.',
-        'url': 'https://github.com/martellaj/repo-stats',
-        'stars': 0,
-        'forks': 0,
-        'owner': 'martellaj',
-        'ownerUrl': 'https://github.com/martellaj'
-      },
-      {
-        'title': 'Office-UI-Fabric',
-        'description': 'The front-end framework for building experiences for Office 365.',
-        'url': 'https://github.com/OfficeDev/Office-UI-Fabric',
-        'stars': 1673,
-        'forks': 182,
-        'owner': 'OfficeDev',
-        'ownerUrl': 'https://github.com/OfficeDev'
-      },
-      {
-        'title': 'repo-stats',
-        'description': 'A sample Office Add-in (built with Angular) that scans emails for links to GitHub repositories and then fetches information about them.',
-        'url': 'https://github.com/martellaj/repo-stats',
-        'stars': 0,
-        'forks': 0,
-        'owner': 'martellaj',
-        'ownerUrl': 'https://github.com/martellaj'
-      },
-      {
-        'title': 'Office-UI-Fabric',
-        'description': 'The front-end framework for building experiences for Office 365.',
-        'url': 'https://github.com/OfficeDev/Office-UI-Fabric',
-        'stars': 1673,
-        'forks': 182,
-        'owner': 'OfficeDev',
-        'ownerUrl': 'https://github.com/OfficeDev'
-      }
-    ]; 
-
-    getDataFromService();
-
-    function getDataFromService(){
-      dataService.getData()
-        .then(function(response){
-          vm.dataObject = response;
-        });
-    }
+    // List of repos mentioned in the email.
+    vm.repos;
+    
+    /////////////////////////////////////////
+		// End of exposed properties and methods. 
+    
+    /**
+     * Apply regular expressions from manifest.xml on message body to get the
+     * URLs that caused the add-in to activate.
+     */
+    var repoUrls = Office.context.mailbox.item.getRegExMatches().GitHubUrl;
+    dataService.getRepoData(repoUrls)
+      .then(function(repoData) {
+        vm.repos = repoData;
+      });
   }
 
 })();
